bff121e59d39dd57e589021746417c61158cda99
Michael Winkler <winklermichael1989@gmail.com>
2025-03-31 09:54:54 +0200

    chore(gradle): upgrade gradle to 8.9.1

--- app/src/main/java/com/nmd/eventCalendar/Utils.kt
+++ app/src/main/java/com/nmd/eventCalendar/Utils.kt
@@ -1,17 +1,10 @@
 package com.nmd.eventCalendar
 
-import android.content.Context
 import androidx.core.graphics.ColorUtils
 
 class Utils {
 
     companion object {
-
-        fun Int.getMonthName(context: Context?): String {
-            context ?: return ""
-            val array = context.resources.getStringArray(R.array.ecv_month_names)
-            return array[this]
-        }
 
         fun Int.isDarkColor(): Boolean {
             return ColorUtils.calculateLuminance(this) < 0.5
